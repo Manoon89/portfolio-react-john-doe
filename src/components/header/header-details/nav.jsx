33ec65b12de10048f7420aec62095231e0b1497d
Manoon <manon.lagniel@gmail.com>
2025-02-27 15:17:00 +0100

    add style to nav.jsx

--- src/components/header/header-details/nav.jsx
+++ src/components/header/header-details/nav.jsx
@@ -3,28 +3,30 @@ import {NavLink} from 'react-router-dom' ;
 export default function Nav () {
 
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary container-fluid">
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark container-fluid">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">John Doe</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand text-light text-uppercase" href="/">John Doe</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end flex-grow-1 pe-3">
                         <li className="nav-item">
-                            <NavLink to ="/">Home</NavLink>
+                            <NavLink className="nav-link text-light text-uppercase" to ="/" >Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to ="/services">Services</NavLink>
+                            <NavLink className="nav-link text-light text-uppercase" to ="/services">Services</NavLink>
                         </li>
                         <li>
-                            <NavLink to ="/portfolio">Portfolio</NavLink>
+                            <NavLink className="nav-link text-light text-uppercase" to ="/portfolio">Portfolio</NavLink>
                         </li>
                         <li>
-                            <NavLink to ="/contact">Contact</NavLink>
+                            <NavLink className="nav-link text-light text-uppercase" to ="/contact">Contact</NavLink>
                         </li>
                         <li>
-                            <NavLink to ="/services">Mentions Légales</NavLink>
+                            <NavLink className="nav-link text-light text-uppercase" to ="/services">Mentions Légales</NavLink>
                         </li>
                     </ul>   
                 </div>
