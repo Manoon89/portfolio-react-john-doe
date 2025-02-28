bdb32fd4ceabb3c438b3fcc0db0230fd408a9372
Manoon <manon.lagniel@gmail.com>
2025-02-28 14:41:14 +0100

    add style on contact page

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -1,13 +1,13 @@
 export default function Form () {
     return (
-        <form>
-            <h3>Formulaire de contact</h3>
-            <input type="text" placeholder="Votre nom" id="name" value="name" required/>
-            <input type="email" placeholder="Votre adresse email" id="email" value="email" required/>
-            <input type="tel" placeholder="Votre numéro de téléphone" id="phone" value="phone" required/>
-            <input type="text" placeholder="Sujet" id="subject" value="subject" required/>
-            <textarea placeholder="Votre message" name="message" id="message" required></textarea>
-            <input type="submit" value="Envoyer" id="Envoyer"/>
+        <form className="d-flex flex-column">
+            <h3 className="border-bottom border-primary pb-4 mb-4 border-3">Formulaire de contact</h3>
+            <input className="form-control p-2 mt-2" type="text" placeholder="Votre nom" id="name" required/>
+            <input className="form-control p-2 mt-2" type="email" placeholder="Votre adresse email" id="email" required/>
+            <input className="form-control p-2 mt-2" type="tel" placeholder="Votre numéro de téléphone" id="phone" required/>
+            <input className="form-control p-2 mt-2" type="text" placeholder="Sujet" id="subject" required/>
+            <textarea className="form-control p-2 mt-2" placeholder="Votre message" name="message" id="message" required></textarea>
+            <input className="btn btn-primary mt-2 align-self-center" type="submit" value="Envoyer" id="Envoyer"/>
         </form>
     )
 }
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -13,22 +13,29 @@ import iconMail from "../assets/icons/envelope-at.svg";
 export default function ContactMe () {
 
     return(
-        <body>
+        <div>
             <Header title="Contact" description="Pour me contacter en vue d'un entretien ou d'une future collaboration,
             merci de remplir le formulaire de contact."/>
-            <main>
-                <Form />
-                <section>
-                    <h3>Mes coordonnées</h3>
-                    <h4>John Doe</h4>
-                    <Address className="text-dark" imgAddress={<img src={iconMap} alt="icone carte"/>} 
-                        imgCity={<img src={iconGeo} alt="icone géolocalisation"/>} 
-                        imgTel={<img src={iconPhone} alt="icone smartphone"/>} 
-                        imgMail={<img src={iconMail} alt="icone enveloppe"/>}
-                    />
-                </section>
+            <main className="w-md-75 shadow p-4 mb-5 bg-body-tertiary rounded container-fluid">
+                <div className="row">
+                    <div className="col-md-6 m-0 p-2">
+                        <Form />
+                    </div>
+                    <div className="col-md-6 m-0 p-2">
+                        <section>
+                            <h3 className="border-bottom border-primary pb-4 mb-4 border-3">Mes coordonnées</h3>
+                            <h4>John Doe</h4>
+                            <Address className="text-dark" imgAddress={<img src={iconMap} alt="icone carte"/>} 
+                                imgCity={<img src={iconGeo} alt="icone géolocalisation"/>} 
+                                imgTel={<img src={iconPhone} alt="icone smartphone"/>} 
+                                imgMail={<img src={iconMail} alt="icone enveloppe"/>}
+                            />
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6618.291437050477!2d4.797268744413279!3d45.77822874451861!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1740748559206!5m2!1sfr!2sfr" width="100%" height="380px" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+                        </section>
+                    </div>
+                </div>
             </main>
             <Footer imgGitHub={imgGit} imgX={imgTwitter} imgLinkedin={imgLink}/>
-        </body>
+        </div>
     );
 }
