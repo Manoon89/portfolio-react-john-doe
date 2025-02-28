8f2b49c65bee757ec661bf1a63574d28b023f74e
Manoon <manon.lagniel@gmail.com>
2025-02-28 11:18:07 +0100

    create address component and import img for address in contact page

--- src/components/sglobals/address.jsx
+++ src/components/sglobals/address.jsx
@@ -1,7 +1,11 @@
-export default function Address () {
+export default function Address (props) {
     return (
-        <div>
-
-        </div>
+        <address>
+            <h3>John Doe</h3>
+            <p>{props.imgAddress} 40 rue Laure Diebold</p>
+            <p>{props.imgCity} 69009 Lyon, France</p>
+            <a href="@tel:1020304050" target="blank">{props.imgTel} 10 20 30 40 50</a>
+            <a href="@mailto:john.doe@gmail.com" target="blank">{props.imgMail} john.doe@gmail.com</a>
+        </address>
     );
 }
