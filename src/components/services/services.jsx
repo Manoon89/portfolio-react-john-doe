d9de907ebde5f7e12efc5b511891d660edcc47c6
Manoon <manon.lagniel@gmail.com>
2025-02-28 21:46:52 +0100

    Change sevices icons color

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,7 +1,7 @@
 export default function Services (props) {
     return (
         <section className="card text-center col-md-3 mb-1 m-2 p-0">
-            <img src={props.img} className="icon-services text-primary card-img-top primary pt-4" alt={props.alt} />
+            {props.img}
             <article className="card-body">
                 <h3 className="card-title">{props.title}</h3>
                 <p className="card-text ">{props.description}</p>
